ed87838cd916986c0253c02a08c4ef40bac908a4
eswaribala <parameswaribala@gmail.com>
2023-05-02 13:05:20 +0530

    updated

---
+++ InventoryAPI/Repositories/CatalogRepository.cs
@@ -0,0 +1,48 @@
+﻿using InventoryAPI.Contexts;
+using InventoryAPI.Models;
+
+namespace InventoryAPI.Repositories
+{
+    public class CatalogRepository : ICatalogRepository
+    {
+        private readonly InventoryContext _dbContext;
+
+        public CatalogRepository(InventoryContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public Catalog AddCatalog(Catalog catalog)
+        {
+            var result = _dbContext.Catalogs.Add(catalog);
+            _dbContext.SaveChanges();
+            return result.Entity;
+        }
+
+        public bool DeleteCatalog(long Id)
+        {
+            var filteredData = _dbContext.Catalogs.Where(x => x.CatalogId == Id).FirstOrDefault();
+            var result = _dbContext.Remove(filteredData);
+            _dbContext.SaveChanges();
+            return result != null ? true : false;
+        }
+
+        public Catalog GetCatalogById(long id)
+        {
+
+            return _dbContext.Catalogs.Where(x => x.CatalogId == id).FirstOrDefault();
+        }
+
+        public IEnumerable<Catalog> GetCatalogList()
+        {
+            return _dbContext.Catalogs.ToList();
+        }
+
+        public Catalog UpdateCatalog(Catalog catalog)
+        {
+            var result = _dbContext.Catalogs.Update(catalog);
+            _dbContext.SaveChanges();
+            return result.Entity;
+        }
+    }
+}
---
+++ InventoryAPI/Repositories/ICatalogRepository.cs
@@ -0,0 +1,13 @@
+﻿using InventoryAPI.Models;
+
+namespace InventoryAPI.Repositories
+{
+    public interface ICatalogRepository
+    {
+        public IEnumerable<Catalog> GetCatalogList();
+        public Catalog GetCatalogById(long id);
+        public Catalog AddCatalog(Catalog catalog);
+        public Catalog UpdateCatalog(Catalog catalog);
+        public bool DeleteCatalog(long Id);
+    }
+}
