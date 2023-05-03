985b4ea95bbce45cc3450524ae8322f50a232495
eswaribala <parameswaribala@gmail.com>
2023-05-03 10:29:00 +0530

    updated

---
+++ InventoryAPI/Controllers/CatalogsController.cs
@@ -0,0 +1,53 @@
+﻿using InventoryAPI.Models;
+using InventoryAPI.Producers;
+using InventoryAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogsController : ControllerBase
+    {
+        private ICatalogRepository catalogRepository;
+        private IRabbitmqProducer rabbitmqProducer;
+
+        public CatalogsController(ICatalogRepository _catalogRepository,
+            IRabbitmqProducer _rabbitmqProducer)
+        {
+            this.catalogRepository = _catalogRepository;
+            this.rabbitmqProducer = _rabbitmqProducer;
+        }
+
+        [HttpGet("cataloglist")]
+        public IEnumerable<Catalog> CatalogList()
+        {
+            var catalogList = catalogRepository.GetCatalogList();
+            return catalogList;
+        }
+        [HttpGet("getcatalogbyid")]
+        public Catalog GetCatalogById(int Id)
+        {
+            return catalogRepository.GetCatalogById(Id);
+        }
+        [HttpPost("addcatalog")]
+        public Catalog AddProduct(Catalog catalog)
+        {
+            var catalogData = catalogRepository.AddCatalog(catalog);
+            //send the inserted product data to the queue and consumer will listening this data from queue
+            rabbitmqProducer.SendMessage(catalogData);
+            return catalogData;
+        }
+        [HttpPut("updatecatalog")]
+        public Catalog UpdateCatalog(Catalog catalog)
+        {
+            return catalogRepository.UpdateCatalog(catalog);
+        }
+        [HttpDelete("deletecatalog")]
+        public bool DeleteCatalog(int Id)
+        {
+            return catalogRepository.DeleteCatalog(Id);
+        }
+    }
+}
--- InventoryAPI/Producers/RabbitmqProducer.cs
+++ InventoryAPI/Producers/RabbitmqProducer.cs
@@ -1,4 +1,6 @@
-﻿using RabbitMQ.Client;
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace InventoryAPI.Producers
 {
@@ -6,26 +8,38 @@ namespace InventoryAPI.Producers
     {
         private ConnectionFactory connectionFactory;
         private IConfiguration configuration;
+        private IConnection connection;
+        private IModel channel;
 
         public RabbitmqProducer(IConfiguration _configuration)
         {
             this.configuration = _configuration;
-            this.configuration = new ConfigurationBuilder()
-                           .AddJsonFile("appsettings.json")
-                           .Build();
+            //this.configuration = new ConfigurationBuilder()
+                //           .AddJsonFile("appsettings.json")
+                 //          .Build();
             connectionFactory = new ConnectionFactory();
             connectionFactory.UserName = configuration["amqp:username"];
             connectionFactory.Password = configuration["amqp:password"];
             connectionFactory.VirtualHost = configuration["amqp:virtualhost"];
             connectionFactory.HostName = configuration["amqp:hostname"];
             connectionFactory.Port = AmqpTcpEndpoint.UseDefaultPort;
-
+            //create the connection
+             connection = connectionFactory.CreateConnection();
+            //create the channel
+            channel = connection.CreateModel();
+            channel.QueueDeclare("CatalogQueue", false, true, true);
         }
 
 
         public void SendMessage<T>(T message)
         {
-            throw new NotImplementedException();
+            
+            var json=JsonConvert.SerializeObject(message);
+            var body=Encoding.UTF8.GetBytes(json);
+            channel.BasicPublish(exchange:"",routingKey:"CatalogQueue",body:body);
+           // channel.Close();
+
+
         }
     }
 }
